531e70af6bb3c13c29d696b5829166f79f0e8c78
anipingping <106618404+anipingping@users.noreply.github.com>
2022-09-02 09:15:07 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -36,16 +36,15 @@ function updateFilters() {
     // 4c. Save the id of the filter that was changed as a variable.
     let filter_id = changed_element.attr("id");
   
-    // 5. If a filter value was entered then add that filterId and value
-    // to the filters list. Otherwise, clear that filter from the filters object.
-    if ("date") {
-      filteredData = filteredData.filter(row => row.datetime === date);
-      filteredData = filteredData.filter(row => row.city === city);
-      filteredData = filteredData.filter(row) => row.state === state);
-      filteredData = filteredData.filter(row) => row.country === country);
-      filteredData = filteredData.filter(row) => row.shape === shape);
-    }
-  
+    // 5. If a filter value was entered then add that filterId and value to the filters list. Otherwise, clear that filter from the filters object.
+if (filters) {
+  changed_element = "123";
+} else if () {
+  abc = "456";
+  else {
+  abc = "789";
+  }
+}
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
   
